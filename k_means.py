e22f3a7200503e2bf654539f3be246d27ee0fd65
Eduardo Antônio de Lucena Lisboa <eduardoalucenal@gmail.com>
2021-09-15 17:40:17 -0300

    Refactoring the used functions
    Refactoring the used functions, minimizing redundant functions and verbose definitions

--- k_means.py
+++ k_means.py
@@ -4,67 +4,77 @@ import numpy as np
 
 
 class KMEANS:
-    def __init__(self, *filename, **image):
-        image_path = image["image"]
+    def __init__(self, image):
+        image_path = image
         bgr_image = cv.imread(image_path)
         self.remove_background(bgr_image)
 
-    def remove_background(self, leaf_image):
-
+    @staticmethod
+    def remove_background(leaf_image):
         # Gaussian blur image to remove noise
         blured = cv.GaussianBlur(leaf_image, (1, 1), 0)
+
+        rgb_image = cv.cvtColor(leaf_image, cv.COLOR_BGR2RGB)
 
         # Convert blured Image from BGR to HSV
         hsv_leaf = cv.cvtColor(blured, cv.COLOR_BGR2HSV)
 
         SV_channel = hsv_leaf.copy()
 
-        SV_channel[:, :, 0] = np.zeros(
-            (SV_channel.shape[0], SV_channel.shape[1]))  # Set the 'H' channel to Zero
+        SV_channel[:, :, 0] = np.zeros((SV_channel.shape[0], SV_channel.shape[1]))  # Set the 'H' channel to zero
 
-        SV_channel[:, :, 2] = np.zeros(
-            (SV_channel.shape[0], SV_channel.shape[1]))
+        SV_channel[:, :, 2] = np.zeros((SV_channel.shape[0], SV_channel.shape[1]))  # Set the 'V' channel to zero
+
         # Create a binary mask from the SV Channel
-
         mask = cv.inRange(SV_channel, (0, 0, 0), (0, 85, 0))
+
         # Invert mask, White areas represent green components and black the background
         mask = cv.bitwise_not(mask)
 
         # perform bitwise_and between mask and hsv image
         background_extracted = cv.bitwise_and(hsv_leaf, hsv_leaf, mask=mask)
 
-        self.segment_diseased(background_extracted)
+        KMEANS.segment_diseased(background_extracted, rgb_image)
 
-    def segment_diseased(self, bg_extracted_hsv):
+    @staticmethod
+    def segment_diseased(bg_extracted_hsv, rgb_image):
         bg_extracted_rgb = cv.cvtColor(bg_extracted_hsv, cv.COLOR_HSV2RGB)
         ycrcb = cv.cvtColor(bg_extracted_rgb, cv.COLOR_RGB2YCrCb)
 
-        r = bg_extracted_rgb[:, :, 0]
-        g = bg_extracted_rgb[:, :, 1]
-        b = bg_extracted_rgb[:, :, 2]
-
-        #ycbcr_image = cv.cvtColor(bg_extracted_rgb, cv.COLOR_RGB2YCrCb)
-        #ycbcr_image[:, :, 0] = np.zeros_like(ycbcr_image[:, :, 0])
-
-        #cr = 128 + (112 * r - 93.786 * g - 18.214 * b) / 256
-
         cr = ycrcb[:, :, 1]
         z = cr.reshape((cr.shape[0] * cr.shape[1]))
-        print(cr.shape)
-        print(z.shape)
+
         # convert to np.float32
         z = np.float32(z)
+
         # define criteria, number of clusters(K) and apply kmeans()
         criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 1)
         K = 2
-        ret, label, center = cv.kmeans(
-            z, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+        ret, label, center = cv.kmeans(z, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+
         # Now convert back into uint8, and make original image
         center = np.uint8(center)
         res = center[label.flatten()]
-        res2 = res.reshape((cr.shape))
+        res2 = res.reshape(cr.shape)
 
-        fig, axs = plt.subplots(1, 2)
-        axs[0].imshow(res2, cmap="gray")
-        axs[1].imshow(cr, cmap="gray")
+        KMEANS.plot_images(rgb_image, cr, res2)
+
+    @staticmethod
+    def plot_images(original, processed, disease_area):
+        fig, axs = plt.subplots(1, 3)
+        axs[0].set_title('Original Image')
+        axs[0].set_xticks([])
+        axs[0].set_yticks([])
+        axs[0].imshow(original, cmap="gray")
+
+        axs[1].set_title('Processed Image')
+        axs[1].set_xticks([])
+        axs[1].set_yticks([])
+        axs[1].imshow(processed, cmap="gray")
+
+        axs[2].set_title('Diseased Area')
+        axs[2].set_xticks([])
+        axs[2].set_yticks([])
+        axs[2].imshow(disease_area, cmap="gray")
+
         plt.show()
--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-from ycbcr import YCBCR
+from ycgcr import YCBCR
 from k_means import KMEANS
 import os
 
 
-class Main:
-    # Path to saved model
-    def getImagePath(self, image_path, filename):
-        # get path of image file
-        return os.path.join(image_path, filename)
+def join_path(imagepath, file):
+    return os.path.join(imagepath, file)
 
-    def getModelPath(self):
-        # get path of model
-        return ("hi")
 
-    def classifyImage(self, imagepath, filename):
-        # Start Image Segmentation
-        # YCBCR(filename, image=self.getImagePath(
-        #    imagepath, filename))
-        KMEANS(filename, image=self.getImagePath(
-            imagepath, filename))
+def main():
+    image_path = 'images'
+    if os.path.exists(image_path):
+        # List all files from a given directory
+        filenames = [file for file in os.listdir(image_path) if os.path.isfile(join_path(image_path, file))]
+
+        # Filter files to only have image files of type JPG, JPEG and PNG
+        image_files = [img_file for img_file in filenames if img_file.split('.')[-1] in ['jpg', 'jpeg', 'png']]
+
+        for img in image_files:
+            full_image_path = os.path.join(image_path, img)
+            KMEANS(full_image_path)
+    else:
+        print("Incorrent path")
 
 
 if __name__ == "__main__":
-    image_path = '/home/frexg/Downloads/lara2018-master/segmentation/dataset/images/test'
-    c = Main()
-    if os.path.exists(image_path):
-        for dirpath, dirname, filenames in os.walk(image_path):
-            for ImageFile in filenames:
-                c.classifyImage(image_path, ImageFile)
-    else:
-        print("Incorrent path")
+    main()
